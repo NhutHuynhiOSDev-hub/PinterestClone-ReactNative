6ff574336b915dc25152f912e19c1cad418fd580
nhuthq <>
2022-04-20 10:54:10 +0700

    handle pin & styles

--- Pinterest/components/Pin.tsx
+++ Pinterest/components/Pin.tsx
@@ -1,37 +1,60 @@
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useState, useEffect } from "react";
 
-import { StyleSheet,Text, View, Image } from "react-native";
+const Pin = (props) => {
+  const { title, image } = props.pin;
+  const onLike = () => {};
+  const [ration, setRation] = useState(1);
 
-const Pin = () => {
-    return(
-        <View style={styles.pin}>
-            <Image
-                style= {styles.image}
-                source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg"}}/>
-            <Text style={styles.title}>notJust Hoodie</Text>
-        </View>
-    );
-}
+  useEffect(() => {
+    if (image) {
+      Image.getSize(image, (width, height) => setRation(width / height));
+    }
+  }, [image]);
+
+  return (
+    <View style={styles.pin}>
+      <View>
+        <Image
+          style={[styles.image, { aspectRatio: ration }]}
+          source={{
+            uri: image,
+          }}
+        />
+        <Pressable onPress={onLike} style={styles.heartBtn}>
+          <AntDesign name="hearto" size={24} color="black" />
+        </Pressable>
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  title: {
+    margin: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 
-    title: {
-        margin: 10,
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
+  pin: {
+    width: "100%",
+  },
 
-    pin: {
+  image: {
+    width: "100%",
+    borderRadius: 25,
+  },
 
-        width: "100%",
-    },
-
-    image: {
-        width: "100%",
-        height: 200,
-        borderRadius: 25,
-    },
+  heartBtn: {
+    backgroundColor: "#d3cfd4",
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    padding: 5,
+    borderRadius: 50,
+  },
 });
 
 export default Pin;
-  
-
--- Pinterest/screens/HomeScreen.tsx
+++ Pinterest/screens/HomeScreen.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, ScrollView } from "react-native";
 
-import Pin from '../components/Pin';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import Pin from "../components/Pin";
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function HomeScreen({ navigation }: RootTabScreenProps<'HomeScreen'>) {
+export default function HomeScreen({
+  navigation,
+}: RootTabScreenProps<"HomeScreen">) {
   return (
     <View style={styles.container}>
-      <Pin/>
+      <ScrollView>
+        <Pin
+          pin={{
+            title: "notJust Dev Hoodie",
+            image:
+              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg",
+          }}
+        />
+        <Pin
+          pin={{
+            title: "Programmer working on laptop computer in office studio",
+            image:
+              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/1.jpeg",
+          }}
+        />
+      </ScrollView>
     </View>
   );
 }
@@ -16,8 +33,8 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'HomeScree
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
   },
 });
