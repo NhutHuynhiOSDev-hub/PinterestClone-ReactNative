519562c8198ab9ffe32814a465aa97195c20845d
nhuthq <>
2022-04-20 14:18:54 +0700

    init PinScreen

---
+++ Pinterest/screens/PinScreen.tsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import { View, Image, StyleSheet, Text, Pressable } from "react-native";
+import pins from "../assets/data/pins";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+
+const PinScreen = () => {
+  const pin = pins[1];
+  const [ration, setRation] = useState(1);
+  const insets = useSafeAreaInsets();
+
+  const goBack = () => {};
+
+  useEffect(() => {
+    if (pin.image) {
+      Image.getSize(pin.image, (width, height) => {
+        setRation(width / height);
+      });
+    }
+  }, [pin.image]);
+
+  return (
+    <SafeAreaView style={{ backgroundColor: "black" }}>
+      <StatusBar style="light" />
+      <View style={styles.root}>
+        <Image
+          source={{ uri: pin.image }}
+          style={[styles.image, { aspectRatio: ration }]}
+        />
+        <Text style={styles.title}>{pin.title}</Text>
+      </View>
+      <Pressable
+        onPress={goBack}
+        style={[styles.backBtn, { top: insets.top + 20 }]}
+      >
+        <Ionicons name="chevron-back" size={35} color="white" />
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    height: "100%",
+    backgroundColor: "white",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  image: {
+    width: "100%",
+    backgroundColor: "white",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  backBtn: {
+    left: 10,
+    position: "absolute",
+  },
+  title: {
+    margin: 10,
+    fontSize: 24,
+    fontWeight: "600",
+    lineHeight: 35,
+    textAlign: "center",
+  },
+});
+
+export default PinScreen;
