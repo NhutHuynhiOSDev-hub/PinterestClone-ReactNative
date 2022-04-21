544fb75f7207835c9658b27ddaee67fe57baeefc
nhuthq <>
2022-04-21 15:42:42 +0700

    init CreatePinScreen & ImagePicker

---
+++ Pinterest/screens/CreatePinScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "../components/Themed";
+
+const CreatePinScreen = () => {
+  return (
+    <View>
+      <Text>This is Create Pin Screen</Text>
+    </View>
+  );
+};
+
+export default CreatePinScreen;
--- Pinterest/types.tsx
+++ Pinterest/types.tsx
@@ -10,7 +10,7 @@ import {
 } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-declare global {
+declare global { 
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
